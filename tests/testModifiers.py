d4b63b6a973ddd40ac2d791bb99bbf2f5f1b6413
jerryc96 <jerryycheng@gmail.com>
2020-06-08 23:22:49 -0400

    added new testcase to check modifierList in case of a game update

---
+++ tests/testModifiers.py
@@ -0,0 +1,25 @@
+import unittest
+from DataBuilder import gen_country_ideas_library
+from ideaGroup.ideaGroup import ideaGroup
+
+class TestModifiers(unittest.TestCase):
+
+    def test_missing_modifiers(self):
+        '''
+        fetch every ideaset in common,
+        Try to convert it into a modifier. If any does not exist in modifier, add it to missing modifiers list.
+        '''
+        missingModifierList = []
+        ideaDict = gen_country_ideas_library()
+
+        for ideaName, ideaSet in ideaDict.items():
+            try:
+                idea = ideaGroup(ideaName, ideaSet)
+            except KeyError as e:
+                if str(e) not in missingModifierList:
+                    missingModifierList.append(str(e))
+        if len(missingModifierList) > 0:
+            print(missingModifierList)
+            self.assertTrue(False)
+        else:
+            self.assertTrue(True)
